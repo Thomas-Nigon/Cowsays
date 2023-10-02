44f6de50095dd229bb63dc8e8d04c82c5ca5ca5d
Thomas Nigon <[nigont@gmail.com](mailto:nigont@gmail.com)>
2023-10-02 10:04:58 +0200

    sss

--- index.js
+++
@@ -1,9 +0,0 @@
-const studentInfo = require('./information'); 
-var cowsay = require("cowsay");
-
-console.log(cowsay.say({
-    text :  `Hi, my name is ${studentInfo.name} and I work at ${studentInfo.campus}`,
-    e : "oO",
-    T : "U "
-   
-}));
